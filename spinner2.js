6608fe497c7e1101c5ba9c82eecbed4e7bf978ea
deepthyp1991 <deepthyp1991@gmail.com>
2020-11-30 20:03:46 +0000

    Adding spinner2.js file and modified spinner1.js file

---
+++ spinner2.js
@@ -0,0 +1,9 @@
+// let spinnerArray = ['\r\ ','\r/ ', '\r- ', '\r\\ ', '\r/ ', '\r\ ', '\n']
+const sniperArr = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\n'];
+let timeInit = -100;
+for (let i = 0; i < 10; i++) {
+  timeInit += 200;
+  setTimeout(() => {
+    process.stdout.write(sniperArr[i]);
+  }, timeInit);
+}
